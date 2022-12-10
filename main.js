97568c4421c5c292dfe7773dfa4da27996c27836
Fulvose <piekosztomasz@gmail.com>
2022-12-11 00:10:01 +0100

    init commit

---
+++ main.js
@@ -0,0 +1,84 @@
+$(".burger").click(function() {
+    $(".side-bar").toggleClass("active");})
+
+$("#fb-icon").click(function() {
+    window.open("https://www.facebook.com/Stowarzyszenie-Tw%C3%B3rczej-Edukacji-Polonistycznej-STEP-102966531552491")})
+
+$("#side-bar-row1").click(function() {
+    location.href = "/o-nas"})
+
+$("#side-bar-row2").click(function() {
+    location.href = "/o-nas"})
+
+$("#side-bar-row3").click(function() {
+    location.href = "/galeria"})
+
+$("#side-bar-row4").click(function() {
+    location.href = "/kontakt"})
+
+$("#content1").click(function() {
+    location.href = "/o-nas"})
+
+$("#content3").click(function() {
+    location.href = "/materialy-dydaktyczne"})
+
+$("#content4").click(function() {
+    location.href = "/konkursy"})
+
+$("#content5").click(function() {
+    location.href = "/publikacje"})
+
+$("#content6").click(function() {
+    location.href = "/kontakt"})
+
+$("#logo").click(function() {
+    location.href = "/home"})
+
+$("#tb2").click(function() {
+    location.href = "/o-nas"})
+
+$("#tb3").click(function() {
+    location.href = "/galeria"})
+
+$("#tb4").click(function() {
+    location.href = "/kontakt"})
+
+$("#logo").click(function() {
+    location.href = "/home"})
+
+$("#show-photo-bg").click(function() {
+    $("#show-photo-bg").css("display", "none");
+})
+
+$("#photo1").click(function() {
+    $("#show-photo").css('background-image', 'url("zdjecia/PLAKAT_styczeń.jpg")');
+    $("#show-photo-bg").css("display", "block");
+    window.scrollTo(0,0);
+})
+
+$("#photo2").click(function() {
+    $("#show-photo").css('background-image', 'url("zdjecia/PLAKAT_marzec.jpg")');
+    $("#show-photo-bg").css("display", "block");
+    window.scrollTo(0,0);
+})
+
+$("#photo3").click(function() {
+    $("#show-photo").css('background-image', 'url("zdjecia/PLAKAT_kwiecień.jpg")');
+    $("#show-photo-bg").css("display", "block");
+    window.scrollTo(0,0);
+})
+
+$("#photo4").click(function() {
+    $("#show-photo").css('background-image', 'url("zdjecia/PLAKAT_maj.jpg")');
+    $("#show-photo-bg").css("display", "block");
+    window.scrollTo(0,0);
+})
+
+$("#photo5").click(function() {
+    $("#show-photo").css('background-image', 'url("zdjecia/PLAKAT_styczeń_2019.jpg")');
+    $("#show-photo-bg").css("display", "block");
+    window.scrollTo(0,0);
+})
+
+
+
